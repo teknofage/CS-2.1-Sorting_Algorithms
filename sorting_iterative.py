ef4c3ba0f4f9c45ef4af68a172eacfeec6ead255
teknofage <teknofage@gmail.com>
2020-10-29 23:03:50 -0700

    finished adding comments

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -7,6 +7,7 @@ def is_sorted(items):
     Why and under what conditions? Not running through any loops and it uses a constant time.
     Memory usage: O(1)    
     Why and under what conditions? No new structures are created or changed so the memory usage is linear."""
+    # make a copy of [sliced]items, sort it, and return True if copy matches items
     copy = items[:]
     copy.sort()
     return copy == items
@@ -19,14 +20,22 @@ def bubble_sort(items):
     Why and under what conditions? Best: single iteration, but Worst would involve several passes of iteration.
     Memory usage: O(1)            
     Why and under what conditions? No new structures are created or changed so the memory usage is linear."""
+    # set is_sorted status to True
     is_sorted = True
+    # create a counter, set to 0
     counter = 0
     while(is_sorted):
+        # while items is_sorted, set is_sorted to False
         is_sorted = False
+        # then loop through items
         for i in range(len(items) - counter - 1):
+            # if an item is larger than the one to the right of it...
             if items[i] > items[i+1]:
+                # then they swap places
                 items[i], items[i+1] = items[i+1], items[i]
+                # and then they become sorted!
                 is_sorted = True
+        # increment the counter to move through the array
         counter += 1
 
 def selection_sort(items):
@@ -48,13 +57,12 @@ def selection_sort(items):
                 min_index = j
         # After finding the lowest item of the unsorted regions, swap with the first unsorted item
         items[i], items[min_index] = items[min_index], items[i]
-"""Now let's add some code to the file to test the algorithm:"""
 
 items = [3, 1, 41, 59, 26, 53, 59]
 print(items)
 selection_sort(items)
 
-# Let's see the list after we run the Selection Sort
+# print the list after we run Selection Sort
 print(items)
 
 
@@ -65,11 +73,16 @@ def insertion_sort(items):
     Why and under what conditions?  Best case only requires linear run time due to small list of items, and Worst case would use a large list of items.
     Memory usage: O(1)            
     Why and under what conditions? No new structures are created or changed so memory usage is minimal. """
+    # set item_length to the range between 1 and length of items
     item_length = range(1, len(items))
+    # loop through item_length
     for i in item_length:
+        # item[i] values start as unsorted values
         unsorted_value = items[i]
-
+        # while the item[-1] values are greater than the unsorted [i] value AND i is greater than 0
         while items[i-1] > unsorted_value and i > 0:
+            # swap the location of the two items
             items[i], items[i-1] = items[i-1], items[i]
+            # decrement i
             i -= 1
     return items
