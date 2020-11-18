6b8ed7b5d7818f2959ba56e5e80be82f61597139
teknofage <teknofage@gmail.com>
2020-11-17 16:50:06 -0800

    added most of time and space complexity comments

--- sorting_integer.py
+++ sorting_integer.py
@@ -4,8 +4,15 @@
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO:   Running time: 
+                O(n+k)
+            Why and under what conditions?
+                It is always linear because ...
+    TODO:   Memory usage: 
+                O(k)
+            Why and under what conditions?
+                Under the worst conditions, 
+            """
     # TODO: Find range of given numbers (minimum and maximum integer values)
     # TODO: Create list of counts with a slot for each number in input range
     # TODO: Loop over given numbers and increment each number's count
@@ -16,8 +23,28 @@ def counting_sort(numbers):
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+       Running time: 
+                Best: 0(n*k) 
+            Why and under what conditions?
+                You only need to traverse the original array once, 
+                and the new array or buckets k times, where k is the number of 
+                buckets that are not empty.
+                
+                Worst: 0(n^2) 
+            Why and under what conditions?
+                There would be the same number of full buckets as there are 
+                items in the original array, or else the array values are so unevenly 
+                distributed that the sorting and arranging of the sub-arrays in
+                the buckets takes up a lot of time
+       Memory usage: 
+                0(n) 
+            Why and under what conditions?
+                Memory usage is minimal and linear, even under the worst 
+                conditions, as the algorithm only needs 
+                to create one extra data structure in the 
+                form of the array of arrays, and in python
+                these are all dynamic arrays which have flexible space.
+            """
     # TODO: Find range of given numbers (minimum and maximum values)
     # TODO: Create list of buckets to store numbers in subranges of input range
     # TODO: Loop over given numbers and place each item in appropriate bucket
